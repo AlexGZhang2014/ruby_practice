8e204ae744cd40e6d291c2fb0e07c19678a6064c
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-19 21:39:09 -0400

    First commit with bundle gem install

---
+++ lib/ruby_practice.rb
@@ -0,0 +1,5 @@
+require "ruby_practice/version"
+
+module RubyPractice
+  # Your code goes here...
+end
---
+++ spec/ruby_practice_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RubyPractice do
+  it "has a version number" do
+    expect(RubyPractice::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
