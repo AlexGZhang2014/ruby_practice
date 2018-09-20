050ad3340d405cab0280b96e6307f50ea1e3d657
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-19 23:37:58 -0400

    Finish the logic for the #movie method

--- lib/ruby_practice/movie_cli.rb
+++ lib/ruby_practice/movie_cli.rb
@@ -24,8 +24,15 @@ class MovieCLI
       movie_num = input.to_i
       if movie_num > 0 && movie_num <= num_of_movies
         movie = movies[movie_num-1]
-        puts "#{movie.name}"
+        puts "Name: #{movie.name}"
         puts "Go to the following link to see the full details of this movie: https://www.regmovies.com#{movie.link}"
+        puts "To see the full list of movies again, enter \'list\'. Otherwise, type \'exit\' when you are done."
+      elsif input == "list"
+        list
+      elsif input == "exit"
+        puts "Thanks for using this app. Goodbye!"
+      else
+        puts "That is not a valid input. You can only type \'list\', \'exit\', or a movie number."
       end
     end
   end
