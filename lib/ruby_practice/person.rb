69f91463f708fcf9aacc23de14775146105d0ac6
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-20 23:58:15 -0400

    Define 3 new methods

--- lib/ruby_practice/person.rb
+++ lib/ruby_practice/person.rb
@@ -12,4 +12,16 @@ class Person
   def self.all
     @@people
   end
+
+  def talk(quote)
+    puts quote
+  end
+
+  def say_my_name
+    puts "My name is #{@name}. Don't forget it!"
+  end
+
+  def full_intro
+    puts "Hello, everyone. My name is #{@name}. I am #{@age} years old. This is my story. #{@bio}"
+  end
 end
